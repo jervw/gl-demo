6c4e222423d0807392c2fa77e022897ab2adfa26
jervw <vuolajere@gmail.com>
2022-11-16 18:18:05 +0200

    added clang-fmt cfg and fixed identationd

--- src/main.cc
+++ src/main.cc
@@ -3,55 +3,55 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void process_input(GLFWwindow *window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_input(GLFWwindow* window);
 
-static void error_callback(int error, const char *description) {
-  fprintf(stderr, "Error %d: %s\n", error, description);
+static void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error %d: %s\n", error, description);
 }
 
 const unsigned WIDTH = 1280;
 const unsigned HEIGHT = 720;
 
-int main(int, char **) {
-  // Setup window
-  glfwInit();
-  glfwWindowHint(GLFW_SAMPLES, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+int main() {
+    // Setup window
+    glfwInit();
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL", NULL, NULL);
-  glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  glewInit();
+    glewInit();
 
-  // main loop
-  while (!glfwWindowShouldClose(window)) {
-    // input
-    process_input(window);
+    // main loop
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        process_input(window);
 
-    // render
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    // glfw swap buffers and poll IO events
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
+        // glfw swap buffers and poll IO events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-  // Cleanup
-  glfwTerminate();
-  return 0;
+    // Cleanup
+    glfwTerminate();
+    return 0;
 }
 
 // process all input
-void process_input(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
